28c71bf6045e4085aa62a7bbdeb349c1c97c2422
engj3ll <arberpr6@gmail.com>
2020-06-28 22:54:49 +0200

    added css to login buttons

--- src/app/Index.java
+++ src/app/Index.java
@@ -26,7 +26,8 @@ public class Index extends VBox {
 	public VBox getIndex() {
 		VBox pane = new VBox(5);
 		pane.setAlignment(Pos.CENTER);
-		pane.setPadding(new Insets(20, 20, 20, 20));
+        pane.setPadding(new Insets(20, 20, 20, 20));
+        //add css to pane
 		pane.setStyle("-fx-background-image: url(\"https://i.imgur.com/OGQJDqs.png\");");
 
 		Login form1 = new Login();
@@ -36,16 +37,15 @@ public class Index extends VBox {
 		
 		HBox buttonPane = new HBox(13);
 		buttonPane.setAlignment(Pos.CENTER);
-		
 		HBox buttonPane1 = new HBox(13);
-		buttonPane1.setAlignment(Pos.CENTER);
+        buttonPane1.setAlignment(Pos.CENTER);
+        //add padding using Insets
 		buttonPane1.setPadding(new Insets(5, 5, 5, 5));
 
 		login = Language.getButton("Button3");
 		signup = Language.getButton("Button4");
-		playasguest = Language.getButton("Button5");
-		
-
+        playasguest = Language.getButton("Button5");
+        //add css to login buttons/signup/playasguest
 		login.setStyle("-fx-text-fill: black; " + "-fx-font-family:'Arial'; "
 				+ "-fx-background-color: linear-gradient(#CACCD1,#F3F4F7\r\n" + 
 				"); "
@@ -56,7 +56,6 @@ public class Index extends VBox {
 		playasguest.setStyle("-fx-text-fill: black; " + "-fx-font-family:'Arial'; "
 				+ "-fx-background-color: linear-gradient(#CACCD1,#F3F4F7); "
 				+ "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );");
-
 		buttonPane.getChildren().add(playasguest);
 		buttonPane1.getChildren().addAll(login, signup);
 		
@@ -75,20 +74,15 @@ public class Index extends VBox {
 			(Main.window).setResizable(false);
 			(Main.window).setTitle("Game");
 		});
-		
 		HBox languageButtons = new HBox(13);
-		
 		Button alBtn = new Button();
 		Button enBtn = new Button();
-		
 		Image alImg = new Image("img/al1.png");
 		Image enImg = new Image("img/en1.png");
 		alBtn.setGraphic(new ImageView(alImg));
 		enBtn.setGraphic(new ImageView(enImg));
-		
 		HBox.setMargin(alBtn, new Insets(5, 1, 5, 4));
 		HBox.setMargin(enBtn, new Insets(5, 5, 5, -3));
-		
 		alBtn.setOnAction(e -> {
 			Language.setLocale(new Locale("al"));
 		});
@@ -96,14 +90,12 @@ public class Index extends VBox {
 		enBtn.setOnAction(e -> {
 			Language.setLocale(new Locale("en"));
 		});
-		
 		languageButtons.getChildren().addAll(alBtn, enBtn);
 		languageButtons.setAlignment(Pos.CENTER);
 		
 		welcome = Language.getLabel("label14");
 		welcome.setPadding(new Insets(3, 3, 3, 3));
 		welcome.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 20));
-
 		pane.getChildren().addAll(welcome, buttonPane, buttonPane1, languageButtons);
 		return pane;
 	}
